ff912f99672bef614cb93e0c2c299ce77e2f061b
omomi-moti <ikpubon@icloud.com>
2025-10-02 17:50:48 +0900

    Login_button

---
+++ karaoke_merged/frontend/src/components/LoginButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { loginWithSpotify } from "../api/spotify";
+
+export default function LoginButton() {
+  return (
+    <button className="btn btn-primary" onClick={loginWithSpotify}>
+      Spotifyでログイン
+    </button>
+  );
+}
