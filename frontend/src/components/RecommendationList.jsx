dbcbd6dd7d8d3ff5cfacfa9ca5a87235f15853b7
STS369 <satoshikikuchi778@gmail.com>
2025-10-04 13:09:55 +0900

    ReccomendationList.jsx_change_list

--- frontend/src/components/RecommendationList.jsx
+++ frontend/src/components/RecommendationList.jsx
@@ -2,12 +2,19 @@
 import React from "react";
 import SongCard from "./SongCard";
 
-export default function RecommendationList({ tracks, featuresMap }) {
+export default function RecommendationList({ tracks, featuresMap, onAddSelected }) {
   if (!tracks?.length) return <div>おすすめを準備中…</div>;
   return (
     <div className="grid">
       {tracks.map(t => (
-        <SongCard key={t.id} track={t} feature={featuresMap?.get(t.id)} popularity={t.popularity} />
+        <div key={t.id} className="rec-card">
+          <SongCard track={t} feature={featuresMap?.get(t.id)} popularity={t.popularity} />
+          {onAddSelected && (
+            <div style={{ marginTop: 8 }}>
+              <button className="btn primary" onClick={() => onAddSelected(t)}>選択に追加</button>
+            </div>
+          )}
+        </div>
       ))}
     </div>
   );
